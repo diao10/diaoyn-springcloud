20239907cefd48eb4e64822c27f782831749c245
刁一宁 <12122319+boring_day@user.noreply.gitee.com>
2024-12-16 17:48:41 +0800

    core

---
+++ diaoyn-common/src/main/java/com/diaoyn/common/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.diaoyn.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 跨域解决
+ *
+ * @author diaoyn
+ * @ClassName CorsConfig
+ * @Date 2024/12/16 15:30
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 对所有路径应用跨域配置
+                .allowedOrigins("*") // 允许任何域进行跨域访问
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 允许的请求方法
+                .allowedHeaders("*"); // 允许的请求头
+    }
+}
