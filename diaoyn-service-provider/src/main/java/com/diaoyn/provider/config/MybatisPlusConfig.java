f4d48498e7a92b4b6e32b8adce425109340fa02c
刁一宁 <12122319+boring_day@user.noreply.gitee.com>
2024-12-17 14:53:06 +0800

    add  MybatisPlusConfig

---
+++ diaoyn-service-provider/src/main/java/com/diaoyn/provider/config/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.diaoyn.provider.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * MybatisPlus配置
+ *
+ * @author EDY
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor()); //分页插件
+//        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor()); //乐观锁插件
+        return interceptor;
+    }
+
+}
