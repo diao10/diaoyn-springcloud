ba90fab296c2b3b01b4f254985c0ce98a5b955d0
刁一宁 <12122319+boring_day@user.noreply.gitee.com>
2025-03-21 10:30:18 +0800

    update

---
+++ diaoyn-service-provider/src/main/java/com/diaoyn/provider/config/GradioConfig.java
@@ -0,0 +1,19 @@
+package com.diaoyn.provider.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author diaoyn
+ * @ClassName GradioConfig
+ * @Date 2025/2/19 14:51
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "gradio")
+public class GradioConfig {
+
+     private String url;
+
+}
