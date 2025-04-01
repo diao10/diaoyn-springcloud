e4cc064bd23f10524c75c90b70e761d38a8a5b1b
刁一宁 <12122319+boring_day@user.noreply.gitee.com>
2025-04-01 17:32:34 +0800

    update

--- abandon/shell/way2/startup.sh
+++ abandon/shell/way2/startup.sh
@@ -2,12 +2,13 @@
 DIR=$(cd "$(dirname "$0")"; pwd)
 PIDFILE="$DIR/server.pid"
 LOGFILE="$DIR/logs/stdout.log"
+JARFILE=$(dirname "$DIR")
 CONFIGPATH="$HOME/config/consumer/"
 
 if [ -f $PIDFILE ] &&  [ $(cat $PIDFILE) -gt 0 ] ;  then
 echo "Application is already running..."
 exit 1
 fi 
-nohup java -Xms256m -Xmx2048m -jar $DIR/diaoyn-service-consumer.jar --spring.profiles.active=prod --spring.config.location=$CONFIGPATH  > $LOGFILE 2>&1 & echo $! > $PIDFILE
+nohup java -Xms256m -Xmx2048m -jar $JARFILE/diaoyn-service-consumer.jar --spring.profiles.active=prod --spring.config.location=$CONFIGPATH  > $LOGFILE 2>&1 & echo $! > $PIDFILE
 echo "Application start..."
 
